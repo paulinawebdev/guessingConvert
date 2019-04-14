b4d217fc7b41291e343bd3c09d2db97f7bcba574
Paulina C <paulinachee.is@gmail.com>
2019-04-13 18:31:45 -0700

    guessing convert - paulina chee

--- oop/main.js
+++ oop/main.js
@@ -1,7 +1,12 @@
 
-
 $(document).ready(startApp);
 
+var guess_start;
+
+var random_num;
+
 function startApp(){
-	
+	random_num = new RandomGenerator(1, 10);
+	guess_start = new GuessingGame();
+	guess_start.guess_start();
 }
